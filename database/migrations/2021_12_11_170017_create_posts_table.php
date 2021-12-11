0c344fa2a3a559e19fd59c46c1b0bdcb6639c588
Ferdous Anam <ferdous.anam@gmail.com>
2021-12-11 23:10:38 +0600

    Project Init

---
+++ database/migrations/2021_12_11_170017_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Category::class)->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->longText('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
